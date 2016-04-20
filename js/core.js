461e9243faa31121688f74546c62ccf21b522c8b
bearcott <bear@bearcott.com>
2016-04-20 18:10:07 -0500

    feat: logo and background animation, moved around stuff

--- js/core.js
+++ js/core.js
@@ -1,19 +1,23 @@
-function bgCanvas() {
-  const canvas = $('canvas.logoJumbo')[0];
-  const ctx = canvas.getContext('2d');
+function baseCanvas(query) {
+  that = {};
+  that.canvas = $(query)[0];
+  that.ctx = that.canvas.getContext('2d');
+  return that;
+}
+function logoCanvas() {
+  const that = baseCanvas('canvas.logoJumbo');
   const logoImg = $('<img/>').attr('src','/img/logo-thin.svg')[0];
   const bgColor = $('body').css('background-color') || '#FFF8E7';
-
-  this.ensureCanvasSize = ()=>{
+  that.ensureCanvasSize = ()=>{
     const width = window.innerWidth;
     const height = window.innerHeight;
     const ratio = window.devicePixelRatio || 1;
     // 1. Ensure the element size stays the same.
-    canvas.style.width = width + 'px';
-    canvas.style.height = height + 'px';
+    that.canvas.style.width = width + 'px';
+    that.canvas.style.height = height + 'px';
     // 2. Increase the canvas dimensions by the pixel ratio.
-    canvas.width = width*ratio;
-    canvas.height = height*ratio;
+    that.canvas.width = width*ratio;
+    that.canvas.height = height*ratio;
   }
   function drawImage() {
     const ratio = window.devicePixelRatio || 1;
@@ -22,12 +26,12 @@ function bgCanvas() {
     const imageH = docH;
     const imageW = docH*logoImg.width/logoImg.height;
 
-    ctx.beginPath();
-    ctx.rect(0,0,docW,docH);
-    ctx.fillStyle = bgColor;
-    ctx.fill();
-    ctx.globalCompositeOperation = 'destination-out';
-    ctx.drawImage(
+    that.ctx.beginPath();
+    that.ctx.rect(0,0,docW,docH);
+    that.ctx.fillStyle = bgColor;
+    that.ctx.fill();
+    that.ctx.globalCompositeOperation = 'destination-out';
+    that.ctx.drawImage(
       logoImg,
       docW-3*imageW/4,
       -docH*.1,
@@ -35,24 +39,76 @@ function bgCanvas() {
       imageH*1.2
     );
   }
-  this.drawImage = ()=>{
+  that.draw = ()=>{
     if (logoImg.complete) drawImage();
-    logoImg.onload = ()=>{
+    logoImg.onload =()=>{
       drawImage();
     }
   }
+  return that;
+}
+
+function bgCanvas() {
+  const that = baseCanvas('canvas.background');
+  const bgImg = $('<img/>').attr('src','/img/background.jpg')[0];
+
+
+  that.ensureCanvasSize = ()=>{
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    that.canvas.width = width;
+    that.canvas.height = height;
+
+    //for that pixel effect
+    that.ctx.mozImageSmoothingEnabled = false;
+    that.ctx.imageSmoothingEnabled = false;
+  }
+  function drawImage(scaleNum, offsetNumX, offsetNumY) {
+    const canW = that.canvas.width*1.1;
+    const canH = that.canvas.height*1.1;
+    const scale = (scaleNum) ? scaleNum : 0.1;
+    const offsetX = (offsetNumX) ? offsetNumX : 0;
+    const offsetY = (offsetNumY) ? offsetNumY : 0;
+
+    that.ctx.drawImage(bgImg,offsetX,offsetY,canW*scale,canH*scale);
+    that.ctx.drawImage(that.canvas,0,0,canW*scale,canH*scale,0,0,canW,canH);
+  }
+  that.draw = ()=>{
+    if (bgImg.complete) drawImage();
+    bgImg.onload = ()=>{
+      drawImage();
+    }
+  }
+  that.shine = ()=>{
+
+  }
+  that.flicker = (rateNum)=>{
+    var i = 0;
+    const rate = (rateNum !== undefined) ? rateNum : 8;
+    that.flickering = setInterval(()=>{
+      drawImage(0.08,Math.cos(i++/rate)/rate,-Math.cos(i/rate)/rate);
+    },120)
+  }
+  that.flicker.stop = ()=>clearInterval(that.flickering)
+  return that;
 }
 
 
 $(function() {
 
-  const can = new bgCanvas();
-  can.ensureCanvasSize();
-  can.drawImage();
+  const logoCan = logoCanvas();
+  const bgCan = bgCanvas();
+  logoCan.ensureCanvasSize();
+  logoCan.draw();
+  bgCan.draw();
+  bgCan.ensureCanvasSize();
+  bgCan.flicker(3);
 
   $(window).on('resize',function() {
-    can.ensureCanvasSize();
-    can.drawImage();
+    logoCan.ensureCanvasSize();
+    logoCan.draw();
+    bgCan.ensureCanvasSize();
+    bgCan.draw();
   })
 
 });
