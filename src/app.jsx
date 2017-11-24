ddf866f2914f13a462e44a9487bb73df0d9c4b1e
bearcott <bear@bearcott.com>
2017-11-24 13:18:52 -0600

    fall2017 a whole new world

---
+++ src/app.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import 'normalize.css';
+import 'styles/index.scss';
+
+export default class App extends React.Component {
+	render() {
+		return (
+			<div className='App'>
+				<div>
+					<h1>It Works!</h1>
+					<p>This React project just works including <span className="redBg">module</span> local styles.</p>
+					<p>Enjoy!</p>
+				</div>
+			</div>
+		)
+	}
+}
